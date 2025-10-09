8d4269f0433be491740a89bc31b79faf996072bc
NIk <127285319+lilnikkk@users.noreply.github.com>
2025-10-09 08:37:02 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 module.exports = {
   bot: {
     token: process.env.token || "", // Bot token
-    clientName: process.env.clientName || "SomeCoolName", // Bot username
+    clientName: process.env.clientName || "Nhot", // Bot username
     clientId: process.env.clientId || "", // Bot clientId
     clientSecret: process.env.clientSecret || "", // Bot clientSecret
   },
@@ -17,25 +17,19 @@ module.exports = {
   botSettings: {
     mongoUrl: process.env.mongoUrl || "", // Mongodb url for database
     geniusToken: process.env.geniusToken || "", // Genius token use to fetch lyrics, you can leave it blank.
-    prefix: process.env.prefix || "?", // Default prefix is set to "?" use prefix command to change
+    prefix: process.env.prefix || ".", // Default prefix is set to "?" use prefix command to change
     embedColor: process.env.embedColor || "2F3136", // You can use any HEX Color but without the "#"
   },
 
   // Lavalink settings. Please use lavalink v4
   // You can remove docker-node if youre not using docker to run the bot.
   nodes: [
+    
     {
-      name: "docker-node",
-      host: "docker.lavalink",
-      password: "youshallnotpass",
-      port: 2333,
-      secure: false,
-    },
-    {
-      name: "node-1",
-      host: "localhost",
-      password: "youshallnotpass",
-      port: 2333,
+      name: "Main",
+      host: "lavalinkv4.serenetia.com",
+      password: "https://dsc.gg/ajidevserver",
+      port: 80,
       secure: false,
     },
   ],
@@ -44,7 +38,7 @@ module.exports = {
   presence: {
     activities: [
       {
-        name: "/play",
+        name: "Nhớt iu em",
         type: ActivityType.Listening,
       },
     ],
