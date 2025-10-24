cfc984f07a12efad8b8261b68ae3ccab4d060115
NIk <127285319+lilnikkk@users.noreply.github.com>
2025-10-25 04:16:54 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -17,7 +17,7 @@ module.exports = {
   botSettings: {
     mongoUrl: process.env.mongoUrl || "", // Mongodb url for database
     geniusToken: process.env.geniusToken || "", // Genius token use to fetch lyrics, you can leave it blank.
-    prefix: process.env.prefix || ".", // Default prefix is set to "?" use prefix command to change
+    prefix: process.env.prefix || "=", // Default prefix is set to "?" use prefix command to change
     embedColor: process.env.embedColor || "2F3136", // You can use any HEX Color but without the "#"
   },
 
